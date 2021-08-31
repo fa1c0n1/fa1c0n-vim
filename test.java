d7cf93a57be40bd939775928f2a26133e7b7272f
m01e <tmacfalcon@gmail.com>
2021-08-31 10:19:20 +0800

    add test axzq

---
+++ test.java
@@ -0,0 +1,10 @@
+import cn.com.essence.Impl.TestExpDemo;
+import cn.com.axzq.level2.CoreMmail;
+
+public class MyPortal {
+
+        public static void main(String[] args) {
+            System.out.println("Hello, essence.com.cn");
+            System.out.println("Hello, axzq.com.cn");
+        }
+}
